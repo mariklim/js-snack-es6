4c2742b9a455f7304b6b089afe1f2c51adeae94a
mariklim <maria.klimenko889@gmail.com>
2021-09-10 09:21:05 +0200

    aggiunto function extractValue

--- snack-2/script.js
+++ snack-2/script.js
@@ -35,28 +35,53 @@ const squadre = [
     },
 ];
 
+
 //3. Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
-const arrFalliSubiti = [];
+
+//1. variante senza il ciclo:
 
 
-    const [obj1,obj2,obj3,obj4] = squadre;
-    var sq1 = obj1;
-    var sq2 = obj2;
-    var sq3 = obj3;
-    var sq4 = obj4;
+// const arrFalliSubiti = [];
+
+    // const [obj1,obj2,obj3,obj4] = squadre;
+    // var sq1 = obj1;
+    // var sq2 = obj2;
+    // var sq3 = obj3;
+    // var sq4 = obj4;
  
 
-    var {nome, falliSubiti}=sq1;
-    arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
+    // var {nome, falliSubiti}=sq1;
+    // arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
 
-    var {nome, falliSubiti}=sq2;
-    arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
+    // var {nome, falliSubiti}=sq2;
+    // arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
 
-    var {nome, falliSubiti}=sq3;
-    arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
+    // var {nome, falliSubiti}=sq3;
+    // arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
 
-    var {nome, falliSubiti}=sq4;
-    arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
+    // var {nome, falliSubiti}=sq4;
+    // arrFalliSubiti.push(`Nome della squadra: ${nome}, falli subiti: ${falliSubiti}`);
 
-    console.log(arrFalliSubiti);
+    // console.log(arrFalliSubiti);
+
+
+// 2. versione con la funzione "extractValue"
+// program to extract value as an array from an array of objects
+
+    function extractValue(arr, prop1, prop2) {
+
+      let arrFalliSubiti = [];
+    
+        for (let i=0; i < arr.length ; ++i) {
+    
+            // extract value from property
+            arrFalliSubiti.push(`nome della squadra: ${arr[i][prop1]}`);
+            arrFalliSubiti.push(`falli subiti: ${arr[i][prop2]}`);
+            // arrFalliSubiti.push(arr[i][prop1]);
+            // arrFalliSubiti.push(arr[i][prop2]);
+        }
+        return arrFalliSubiti;
+    }
+
+    console.log(extractValue(squadre,"nome","falliSubiti"));
